c510cc8e2c3c05ee15538dfaabf01e353bb4e249
Przemek Gorecki <przemyslaw.gorecki93@gmail.com>
2020-02-12 22:19:53 +0100

    Separate class

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,6 +1,6 @@
-import {select, classNames, settings, templates} from './settings.js';
-import utils from './utils.js';
-import CartProduct from './components/CartProduct.js';
+import {select, classNames, settings, templates} from '../settings.js';
+import utils from '../utils.js';
+import CartProduct from './CartProduct.js';
 
 class Cart {
   constructor(element) {
