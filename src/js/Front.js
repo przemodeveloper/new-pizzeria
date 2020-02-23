fc1da986567f4b40ddad1ee27bdc9e3274e1610d
Przemek Gorecki <przemyslaw.gorecki93@gmail.com>
2020-02-23 11:01:29 +0100

    Fix page

--- src/js/Front.js
+++ src/js/Front.js
@@ -6,27 +6,27 @@ redirect();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName('mySlides');
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = 'block';
   setTimeout(showSlides, 3000);
 } 
-/* eslint-disable */
+/* eslint-enable */
 
 function redirect() {
-  const orderButton = document.getElementById("order-button");
-  const bookButton = document.getElementById("book-button");
+  const orderButton = document.getElementById('order-button');
+  const bookButton = document.getElementById('book-button');
 
   orderButton.addEventListener('click', function() {
-    window.location.href = "front.html#/order";
+    window.location.href = 'front.html#/order';
 
   });
 
   bookButton.addEventListener('click', function() {
-    window.location.href = "front.html#/booking";
+    window.location.href = 'front.html#/booking';
   });
 }
